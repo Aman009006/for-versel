d656fea7ba40e2970d0ebbbe0415ea1ebfbfcf41
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2020-10-19 15:10:38 +0200

    [REFACTOR] renamed function - name

--- src/api/skillsWithIntents.js
+++ src/api/skillsWithIntents.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 
-export async function getSkills() {
-// extract domain in const
+export async function getSkillsWithIntents() {
+  // extract domain in const
   const res = await request({
     url: 'https://adminbackend.stadtwerk.bot/skillsWithIntentNames',
     method: 'get'
