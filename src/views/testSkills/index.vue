716e32d65e03b9fe5c49e9bb27eba63235839392
Valeriya Bechberger <v.bechberger@hsag.info>
2020-10-22 16:43:48 +0200

    [TASK] load skills danamically in the cascader in testSkills

---
+++ src/views/testSkills/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-if="dataReady">
+      <el-cascader-panel :options="options" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { getSkillsWithIntents } from '@/api/skillsWithIntents'
+export default {
+  name: 'TestSkills',
+  props: {},
+  data() {
+    return {
+      dataReady: false,
+      skill: 'default',
+      options: []
+    }
+  },
+  computed: {},
+  created() {},
+
+  async mounted() {
+    const skillsWithIntents = await getSkillsWithIntents()
+    skillsWithIntents.forEach(skillWithIntent => {
+      this.options.push({ value: skillWithIntent.SkillName, label: skillWithIntent.SkillName })
+      skillWithIntent.IntentNames.forEach(intentName => {
+        // to fill the children
+      })
+    })
+    this.dataReady = true
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
