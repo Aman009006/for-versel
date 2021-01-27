843175df8dbd50c27c5e4bac2d046acc2648eed5
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2021-01-27 14:02:19 +0100

    [TASK] added description - box in dashboard

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,31 +1,68 @@
 <template>
   <div class="dashboard-container">
-    <component :is="currentRole" />
+    <div class="description">
+      <el-row>
+        <el-col :span="24" :sm="2" :lg="1">
+          <img
+            src="https://fileupload.h-s-a-g.de/chatbot/demo/ChatbotAvatar1.svg"
+          >
+        </el-col>
+        <el-col :span="24" :sm="20">
+          <h1>Willkommen zurück auf Ihrer Admin UI!</h1>
+          Es handelt sich hierbei eine <strong>Beta-Version</strong> unserer
+          Admin UI. Die wesentlichen Funktionen sind bereits implementiert, die
+          vollumfängliche Entwicklung der einzelnen Funktionen ist jedoch noch
+          nicht abgeschlossen. Daher ist uns für die Weiterentwicklung und
+          Optimierung unserer Admin UI Ihr Feedback besonders wichtig!
+          <p>Ihr Chatbot Team :)</p>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import adminDashboard from './admin'
-import editorDashboard from './editor'
+import { mapGetters } from "vuex";
 
 export default {
-  name: 'Dashboard',
-  components: { adminDashboard, editorDashboard },
+  name: "Dashboard",
+  components: {},
   data() {
-    return {
-      currentRole: 'adminDashboard'
-    }
+    return {};
   },
   computed: {
-    ...mapGetters([
-      'roles'
-    ])
+    ...mapGetters(["roles"]),
   },
-  created() {
-    if (!this.roles.includes('admin')) {
-      this.currentRole = 'editorDashboard'
+  created() {},
+};
+</script>
+
+<style lang="scss">
+@import "~@/styles/element-variables.scss";
+.dashboard-container {
+  padding: 30px 50px;
+  .description {
+    background-color: white;
+    padding: 15px 25px;
+    border-radius: 20px;
+
+    @media (max-width: $--sm - 1) {
+      > * {
+        text-align: center;
+      }
+    }
+    .el-row {
+      .el-col:first-child {
+        img {
+          max-width: 50px;
+        }
+      }
+    }
+
+    h1 {
+      margin-top: 0px;
+      font-size: 20px;
     }
   }
 }
-</script>
+</style>
