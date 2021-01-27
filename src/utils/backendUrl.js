939119528507bf9f2c3fa5b50154975fcbad6737
Valeriya Bechberger <v.bechberger@hsag.info>
2021-01-27 09:52:52 +0100

    [BUGFIX] Backend url is back to normal.

--- src/utils/backendUrl.js
+++ src/utils/backendUrl.js
@@ -2,7 +2,7 @@
 /**
  * the url of the backend based on the environment.
  */
-const backendUrl = process.env.NODE_ENV === 'development' ? 'https://adminbackend.stadtwerk.bot/' : 'https://adminbackend.stadtwerk.bot/'
+const backendUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://adminbackend.stadtwerk.bot/'
 export default backendUrl
 
 /**
