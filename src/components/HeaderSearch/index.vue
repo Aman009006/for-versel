2f71265ca3324088ec32ebafad1dd450a75d1a09
Valeriya Bechberger <v.bechberger@hsag.info>
2021-02-15 14:05:43 +0100

    [TASK] Comments to the fuzzy searching which is currently used.

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -18,8 +18,7 @@
 </template>
 
 <script>
-// fuse is a lightweight fuzzy-search module
-// make search results more in line with expectations
+// Fuzzy searching finds strings that are approximately equal to a given pattern
 import Fuse from 'fuse.js'
 import path from 'path'
 
@@ -39,6 +38,7 @@ export default {
       return this.$store.getters.permission_routes
     }
   },
+  // watch is lazy by default, i.e. the callback is only called when the watched source has changed.
   watch: {
     routes() {
       this.searchPool = this.generateRoutes(this.routes)
@@ -73,20 +73,43 @@ export default {
       this.$router.push(val.path)
       this.search = ''
       this.options = []
+      // nextTick is a comfortable way to execute a function after the data has been set and the DOM has updated.
       this.$nextTick(() => {
+        // Inside the callback, the DOM has been updated so we can interact with the “most updated” version of it.
         this.show = false
       })
     },
     initFuse(list) {
+      /**
+       * Fuse gets two arguments: list (collection where the search happens)
+       * and options
+       */
       this.fuse = new Fuse(list, {
         shouldSort: true,
+        /**
+         * Defines when the match algorithm gives up:
+         * 0.0 requires a perfect match of both letters and location
+         * 1.0 would match anything
+         */
         threshold: 0.4,
-        location: 0,
-        distance: 100,
+        // Determines approximately where the pattern is expected to be found in the text.
+        location: 0, // default 0
+        /**
+         * Determines how close the match must be to the fuzzy location specified by location:
+         * A distance of 1000 would require a perfect match to be within 800 characters
+         * of the location to be found using a threshold of 0.8.
+         */
+        distance: 100, // default 100
         maxPatternLength: 32,
+        /**
+         * Only the matches whose length exceeds this value will be returned.
+         * If you want to ignore single character matches in the result, set it to 2.
+         */
         minMatchCharLength: 1,
+        // List of keys that will be searched.
         keys: [{
           name: 'title',
+          // weighted search
           weight: 0.7
         }, {
           name: 'path',
@@ -130,6 +153,7 @@ export default {
     },
     querySearch(query) {
       if (query !== '') {
+        // get the result of the search
         this.options = this.fuse.search(query)
       } else {
         this.options = []
