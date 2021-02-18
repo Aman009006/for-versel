ddfaf42112b115a38ec3b6b534a82b29a9c02c02
Valeriya Bechberger <v.bechberger@hsag.info>
2021-02-18 17:34:14 +0100

    [TASK] Got the link to power bi dynamically and added it to the path.

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@
 export const links = {
     jiraHelpDesk: 'https://hsagbpo.atlassian.net/servicedesk/customer/portal/8',
     releaseNotes: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/21004395/Release+Notes+Chatbot+Isa',
-    manual: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/57803205/Bedienungsanleitung+Admin+UI'
+    manual: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/57803205/Bedienungsanleitung+Admin+UI',
 }
