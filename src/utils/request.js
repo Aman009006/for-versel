febb65db8f998462fa6af8de1d8b6cf9a2751751
Valeriya Bechberger <v.bechberger@hsag.info>
2021-12-13 12:22:46 +0100

    [TASK] Unused code is removed

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
-import store from '@/store'
 // import { getToken } from '@/utils/auth'
 import router from '@/router'
 
@@ -14,13 +13,7 @@ const service = axios.create({
 // request interceptor
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
-
-    if (store.getters.token) {
-      // let each request carry token
-      // https://jwt.io/introduction/
-      // config.headers['Authorization'] = 'Bearer ' + getToken()
-    }
+    // here you can do something before request is sent
     return config
   },
   error => {
