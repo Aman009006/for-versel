ef2de2ee776c16871b748f0c721a749646cdc4a7
Nishara <n.singh@hsag.info>
2022-02-02 15:17:02 +0100

    New Link Admin UI

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ export const links = {
     jiraHelpDesk: 'https://hsagbpo.atlassian.net/servicedesk/customer/portal/8',
     releaseNotes: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/21004395/Release+Notes+Chatbot+Isa',
     manual: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/57803205/Bedienungsanleitung+Admin+UI',
+    manualChatbot: 'https://hsagsoftware.atlassian.net/wiki/spaces/CHAT/pages/417890750/API-Dokumentation+Chatbot+Frontend'
 }
 
 export const paths = {
