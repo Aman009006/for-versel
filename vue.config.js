b639dfe61cf32da7431b55cf42e9c76982c7926a
mschuelein <m.schuelein@hsag.info>
2022-04-26 13:00:39 +0200

    [TASK] fix for hot loading and small update

--- vue.config.js
+++ vue.config.js
@@ -54,20 +54,6 @@ module.exports = {
     devtool: 'source-map'
   },
   chainWebpack(config) {
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    // it can improve the speed of the first screen, it is recommended to turn on preload
-    /* @todo
-    config.plugin('preload').tap(() => [
-      {
-        rel: 'preload',
-        // to ignore runtime.js
-        // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
-        fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
-        include: 'initial'
-      }
-    ])
-    */
-
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
 
