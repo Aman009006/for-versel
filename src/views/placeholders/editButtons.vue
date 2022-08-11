dd07828924653983a89e7b7e9ce1909fad578135
mschuelein <m.schuelein@hsag.info>
2022-08-11 11:20:32 +0200

    [TASK] added edit buttons to table

---
+++ src/views/placeholders/editButtons.vue
@@ -0,0 +1,88 @@
+<template>
+    <el-table-column align="center" width="130">
+        <template slot-scope="{row}">
+            <!-- When the editing mode is turned on: -->
+            <template v-if="row.edit">
+                <el-button class="confirm-btn" size="small" icon="el-icon-download" @click="confirmEdit(row)">
+                    Speichern
+                </el-button>
+                <el-button class="cancel-btn" size="small" icon="el-icon-refresh" @click="cancelEdit(row)">
+                    Abbrechen
+                </el-button>
+            </template>
+            <!-- When the editing mode is turned off: -->
+            <template v-else>
+                <el-button class="edit-btn" size="small" icon="el-icon-edit" @click="toggleEdit(row)">
+                    Bearbeiten
+                </el-button>
+            </template>
+        </template>
+    </el-table-column>
+</template>
+
+<script>
+export default {
+    methods: {
+        async toggleEdit(row) {
+            row.edit = !row.edit;
+        },
+        async cancelEdit(row) {
+            row.edit = false;
+
+            this.$message({
+                message: 'Die Änderungen wurden abgebrochen.',
+                type: 'warning'
+            })
+        },
+        async confirmEdit(row) {
+
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.cancel-btn {
+    color: white !important;
+    background-color: #fa4c4c !important;
+    border-color: #fa4c4c !important;
+}
+
+.cancel-btn:hover {
+    color: white !important;
+    background-color: #f78989 !important;
+    border-color: #f78989 !important;
+}
+
+.confirm-btn {
+    color: white !important;
+    background-color: #63c731 !important;
+    border-color: #63c731 !important;
+}
+
+.confirm-btn:hover {
+    color: white !important;
+    background-color: #85ce61 !important;
+    border-color: #85ce61 !important;
+}
+
+.edit-btn {
+    border-radius: 9px !important;
+    margin-left: 0px !important;
+    width: 100%;
+
+    +button {
+        margin-top: 5px;
+    }
+
+    border: solid #409eff 2px !important;
+    color: white !important;
+    background-color: #409eff !important;
+}
+
+.edit-btn:hover {
+    border: solid #66b1ff 2px !important;
+    color: white !important;
+    background-color: #66b1ff !important;
+}
+</style>
--- src/views/placeholders/placeholderTable.vue
+++ src/views/placeholders/placeholderTable.vue
@@ -1,5 +1,86 @@
 <template>
-    <div>
-        placeholderTable
+    <div class="table-container">
+        <template v-if="dataReady">
+            <el-table :data="placeholders" class="placeholder_table" border>
+                <el-table-column align="center" label="Platzhalterbezeichnung" prop="placeholderKey" autosize />
+                <el-table-column align="center" label="Wert" prop="placeholderValue" autosize>
+                    <template slot-scope="{row}">
+                        <template v-if="row.edit">
+                            <el-input v-model="row.placeholderValue" type="textarea" autosize class="edit-input" />
+                        </template>
+                        <template v-else>
+                            <span class="text-input">{{ row.placeholderValue }}</span>
+                        </template>
+                    </template>
+                </el-table-column>
+                <editButtons />
+            </el-table>
+        </template>
     </div>
 </template>
+
+<script>
+import editButtons from './editButtons.vue'
+
+export default {
+    components: {
+        editButtons
+    },
+    data() {
+        return {
+            dataReady: false,
+            placeholders: []
+        }
+    },
+    async created() {
+        /**
+         * Fetch the data when the view is created
+         * and the data is already being observed
+         */
+        await this.loadData()
+    },
+    methods: {
+        async loadData() {
+            const placeholderData = [
+                {
+                    placeholderKey: 'offshore',
+                    placeholderValue: '0,419'
+                },
+                {
+                    placeholderKey: 'year',
+                    placeholderValue: '2022'
+                }
+            ]
+
+            for (const data of placeholderData) {
+                this.$set(data, 'edit', false);
+                this.$set(data, 'originalValue', data.placeholderValue);
+            }
+
+            this.placeholders = placeholderData;
+            this.dataReady = true;
+        }
+    }
+}
+</script>
+
+
+<style scoped lang="scss">
+.el-table {
+    font-size: 13px !important;
+}
+
+.table-container {
+    position: relative;
+    // The chatbot, which has a fixed position, should not cover the table
+    margin-bottom: 120px;
+
+    .disabled-layer {
+        width: 100%;
+        height: 100%;
+        z-index: 2;
+        position: absolute;
+        background-color: rgb(239 239 239 / 77%);
+    }
+}
+</style>
