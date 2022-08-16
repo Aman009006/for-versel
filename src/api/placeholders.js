799fd9adc759c39be363b8f468a2962589e9cdad
mschuelein <m.schuelein@hsag.info>
2022-08-16 16:31:37 +0200

    [TASK] added InfoBox and types

--- src/api/placeholders.js
+++ src/api/placeholders.js
@@ -1,6 +1,9 @@
 import request from '@/utils/request'
 import { authorizedBackendUrl } from '@/utils/backendUrl'
 
+/**
+ * @returns {Placeholder[]} all placeholders from the database
+ */
 export async function getPlaceholders() {
     return request({
         url: authorizedBackendUrl + 'placeholders',
@@ -8,6 +11,10 @@ export async function getPlaceholders() {
     })
 }
 
+/**
+ * @param {string} key 
+ * @param {string} value 
+ */
 export async function setPlaceholder(key, value) {
     return request({
         url: authorizedBackendUrl + 'placeholders',
@@ -19,6 +26,10 @@ export async function setPlaceholder(key, value) {
     })
 }
 
+/**
+ * @param {string} key 
+ * @param {string} value 
+ */
 export async function deletePlaceholder(key, value) {
     return request({
         url: authorizedBackendUrl + 'placeholders',
@@ -30,6 +41,11 @@ export async function deletePlaceholder(key, value) {
     })
 }
 
+/**
+ * @param {string} key 
+ * @param {string} value 
+ * @param {string} oldKey 
+ */
 export async function updatePlaceholder(key, value, oldKey) {
     return request({
         url: authorizedBackendUrl + 'placeholders',
@@ -41,3 +57,9 @@ export async function updatePlaceholder(key, value, oldKey) {
         }
     })
 }
+
+/**
+ * @typedef {Object} Placeholder 
+ * @property {string} key
+ * @property {string} value
+ */
--- src/views/placeholders/placeholderInfoBox.vue
+++ src/views/placeholders/placeholderInfoBox.vue
@@ -1,3 +1,11 @@
 <template>
-    <h1>placeholderInfoBox</h1>
+    <div class="placeholderInfoBox">
+        <el-row>
+            <span style="font-size:14px">
+                Um die Dialoge zu finden, in denen die hier aufgeführten Platzhalter verwendet werden, muss die Suchfunktion genutzt werden.
+                Die Platzhalter müssen im folgenden Format eingegeben werden:
+            </span>
+            <p style="font-size:14px"><span style=" color:#0DA4F0; font-weight: bold;text-transform: uppercase;font-size:13px">##[PLATZHALTERNAME]##</span></p>
+        </el-row>
+    </div>
 </template> 
