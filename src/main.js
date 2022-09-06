6ca20be02e88e235a8849cd4f8813e2b1c0f84c4
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2022-09-06 15:00:08 +0200

    [TASK] created applicationCreator

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
-import { createApp } from 'vue'
-
-import Element from 'element-plus'
 import './styles/element-variables.module.scss'
-import enLang from 'element-plus/es/locale/lang/en'
-
 import '@/styles/index.scss'
-
-import App from './App'
-import store from './store'
-import router from './router'
-
 import './icons'
+import ApplicationCreator from '@/application/ApplicationCreator'
 import './permission'
 
-import SvgIcon from '@/components/SvgIcon'
-
-const app = createApp(App);
-app.use(Element, {
-  size: 'default',
-  locale: enLang,
-});
-app.use(store);
-app.use(router);
-
-app.mount('#app')
-
-app.component('SvgIcon', SvgIcon)
+const application = ApplicationCreator.createApp();
+application.mount('#app');
