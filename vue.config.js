e42a18ec57565888fe3ecfce9cd31fa97b609005
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2022-09-07 14:00:59 +0200

    [TASK] removed unneeded components

--- vue.config.js
+++ vue.config.js
@@ -93,26 +93,6 @@ module.exports = {
     devtool: 'source-map'
   },
   chainWebpack(config) {
-    /**
-     * The compatConfig breaks the login.
-     * With vue 3 without compat, it's working
-     */
-    // config.resolve.alias.set('vue', '@vue/compat')
-
-    // config.module
-    //   .rule('vue')
-    //   .use('vue-loader')
-    //   .tap(options => {
-    //     return {
-    //       ...options,
-    //       compilerOptions: {
-    //         compatConfig: {
-    //           MODE: 2
-    //         }
-    //       }
-    //     }
-    //   })
-
     ckeditorConfig(config)
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
