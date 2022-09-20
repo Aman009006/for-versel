d211d82fd66515eb20e2ac62f40f6a76e4502a8f
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2022-09-20 15:10:31 +0200

    [TASK] fixed login icons

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,49 +1,14 @@
 <template>
-  <div
-    v-if="isExternal"
-    :style="styleExternalIcon"
-    class="svg-external-icon svg-icon"
-  />
-  <svg v-else :class="svgClass" aria-hidden="true">
-    <use :xlink:href="iconName" />
-  </svg>
+  <div class="svg-icon" aria-hidden="true" v-html="svgIconHtml" />
 </template>
 
 <script>
-// doc: https://panjiachen.github.io/vue-element-admin-site/feature/component/svg-icon.html#usage
-import { isExternal } from "@/utils/validate";
-
 export default {
   name: "SvgIcon",
   props: {
-    iconClass: {
+    svgIconHtml: {
       type: String,
       required: true,
-    },
-    className: {
-      type: String,
-      default: "",
-    },
-  },
-  computed: {
-    isExternal() {
-      return isExternal(this.iconClass);
-    },
-    iconName() {
-      return `#icon-${this.iconClass}`;
-    },
-    svgClass() {
-      if (this.className) {
-        return "svg-icon " + this.className;
-      } else {
-        return "svg-icon";
-      }
-    },
-    styleExternalIcon() {
-      return {
-        mask: `url(${this.iconClass}) no-repeat 50% 50%`,
-        "-webkit-mask": `url(${this.iconClass}) no-repeat 50% 50%`,
-      };
     },
   },
 };
@@ -51,16 +16,10 @@ export default {
 
 <style scoped>
 .svg-icon {
-  width: 1em;
-  height: 1em;
+  width: 100%;
+  height: 100%;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
-}
-
-.svg-external-icon {
-  background-color: currentColor;
-  mask-size: cover !important;
-  display: inline-block;
 }
 </style>
--- src/icons/index.js
+++ src/icons/index.js
@@ -1,4 +1,101 @@
+import icon404 from './svg/404.svg?raw';
+import bug from './svg/bug.svg?raw';
+import chart from './svg/chart.svg?raw';
+import clipboard from './svg/clipboard.svg?raw';
+import comment from './svg/comment.svg?raw';
+import component from './svg/component.svg?raw';
+import dashboard from './svg/dashboard.svg?raw';
+import documentation from './svg/documentation.svg?raw';
+import drag from './svg/drag.svg?raw';
+import edit from './svg/edit.svg?raw';
+import education from './svg/education.svg?raw';
+import email from './svg/email.svg?raw';
+import example from './svg/example.svg?raw';
+import excel from './svg/excel.svg?raw';
+import exitFullscreen from './svg/exit-fullscreen.svg?raw';
+import external_link from './svg/external_link.svg?raw';
+import eyeOpen from './svg/eye-open.svg?raw';
+import eye from './svg/eye.svg?raw';
+import form from './svg/form.svg?raw';
+import fullscreen from './svg/fullscreen.svg?raw';
+import guide from './svg/guide.svg?raw';
+import home_filled from './svg/home_filled.svg?raw';
+import icon from './svg/icon.svg?raw';
+import international from './svg/international.svg?raw';
+import language from './svg/language.svg?raw';
+import link from './svg/link.svg?raw';
+import list from './svg/list.svg?raw';
+import lock from './svg/lock.svg?raw';
+import message from './svg/message.svg?raw';
+import money from './svg/money.svg?raw';
+import nested from './svg/nested.svg?raw';
+import password from './svg/password.svg?raw';
+import pdf from './svg/pdf.svg?raw';
+import people from './svg/people.svg?raw';
+import peoples from './svg/peoples.svg?raw';
+import qq from './svg/qq.svg?raw';
+import search from './svg/search.svg?raw';
+import shopping from './svg/shopping.svg?raw';
+import size from './svg/size.svg?raw';
+import skill from './svg/skill.svg?raw';
+import star from './svg/star.svg?raw';
+import tab from './svg/tab.svg?raw';
+import table from './svg/table.svg?raw';
+import theme from './svg/theme.svg?raw';
+import treeTable from './svg/tree-table.svg?raw';
+import tree from './svg/tree.svg?raw';
+import user from './svg/user.svg?raw';
+import wechat from './svg/wechat.svg?raw';
+import zip from './svg/zip.svg?raw';
 
-const req = require.context('./svg', false, /\.svg$/)
-const requireAll = (requireContext) => requireContext.keys().map(requireContext)
-requireAll(req)
+export default {
+    icon404,
+    bug,
+    chart,
+    clipboard,
+    comment,
+    component,
+    dashboard,
+    documentation,
+    drag,
+    edit,
+    education,
+    email,
+    example,
+    excel,
+    exitFullscreen,
+    external_link,
+    eyeOpen,
+    eye,
+    form,
+    fullscreen,
+    guide,
+    home_filled,
+    icon,
+    international,
+    language,
+    link,
+    list,
+    lock,
+    message,
+    money,
+    nested,
+    password,
+    pdf,
+    people,
+    peoples,
+    qq,
+    search,
+    shopping,
+    size,
+    skill,
+    star,
+    tab,
+    table,
+    theme,
+    treeTable,
+    tree,
+    user,
+    wechat,
+    zip
+}
