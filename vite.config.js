2f67dac2ffb5ca6a24a81ce375fc3c20ecbd6c4b
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2022-09-20 15:45:13 +0200

    [TASK] use 9527 as port and show localhost instead of wildcard - ip

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dns from 'dns'
 import { resolve } from 'path';
+
+useLocalHost();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +13,12 @@ export default defineConfig({
             "@": resolve(__dirname, "./src"),
         },
     },
+    server: {
+        port: 9527,
+        strictPort: true
+    }
 })
+
+function useLocalHost() {
+    dns.setDefaultResultOrder('verbatim');
+}
