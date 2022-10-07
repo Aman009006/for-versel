650c11c31aa55f3777f8ebd3829d68c31a56d8e7
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2022-10-07 15:58:08 +0200

    [TASK] created test for description in DialogInfoBox

---
+++ test/components/Dialogs/DialogInfoBox/index.spec.js
@@ -0,0 +1,26 @@
+import DialogInfoBox from '../../../../src/components/Dialogs/DialogInfoBox/index.vue'
+import { expect, it, describe } from 'vitest';
+import VueTestUtils from '../../../utils/VueTestUtils';
+
+describe('DialogInfoBox', async () => {
+    it('should show provided description', function () {
+        const description = "this is my test - description";
+        const testObject = createTestObjectWithDescription(description);
+        const descriptionElement = testObject.element.querySelector('.description');
+        expect(descriptionElement.innerHTML).to.equal(description);
+    });
+})
+
+function createTestObjectWithDescription(description) {
+    const testUtils = createVueTestUtil();
+    const testObject = testUtils.createTestObject({
+        props: {
+            description
+        }
+    });
+    return testObject;
+}
+
+function createVueTestUtil() {
+    return new VueTestUtils(DialogInfoBox);
+}
