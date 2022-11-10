f404e2dd74ddf462c883b5f4c7726e209b2ca478
d.reichler <d.reichler@hsag.info>
2022-11-10 14:19:56 +0100

    [TASK] implemented automated EditAnswerTests

--- test/specs/components/Dialogs/EditAnswer/index.spec.js
+++ test/specs/components/Dialogs/EditAnswer/index.spec.js
@@ -1,39 +1,18 @@
-// import EditAnswer from '../../../../../src/components/Dialogs/EditAnswer/index.vue';
-// import { expect, it, describe } from 'vitest';
-// import VueTestUtils from '../../../../utils/VueTestUtils';
+import { expect, it, describe } from 'vitest';
+import { answerConfigSimple, answer, answerConfigMulti } from '../../../../mocks/answerMocks';
+import EditAnswerTestUtils from './EditAnswerTestUtils';
 
-// const answerConfig = {
-//     type: 'simple',
-//     readable_redirect_to_intent_name: null
-// }
+describe('check ButtonTable in EditAnswer', async () => {
+    it('intent with type simple doesnt contain a ButtonTable', function () {
+        const testObject = EditAnswerTestUtils.createTestObject(answerConfigSimple, answer);
+        const divButtonTable = testObject.element.querySelector("#tableButtonContainer")
+        expect(divButtonTable).to.be.null
+    });
 
-// const answer = {
-//     id: 116467,
-//     text: 'Auf Wiedersehen 🤗',
-//     readableName: null,
-//     description: null,
-//     buttons: [{ type: 'imBack', title: 'test', value: 'test', answer_config: 82030, identificator: null }]
-// }
-
-// describe('check EditAnswers', async () => {
-//     it('should contain an ButtonTable', function () {
-//         const testObject = createTestObjectEditAnswer();
-//         console.log(testObject)
-//     });
-// })
-
-// function createTestObjectEditAnswer() {
-//     const testUtils = createVueTestUtil();
-//     const testObject = testUtils.createTestObject({
-//         props: {
-//             answer,
-//             answerConfig
-//         }
-//     });
-//     return testObject;
-// }
-
-// function createVueTestUtil() {
-//     return new VueTestUtils(EditAnswer);
-// }
+    it('intent with type multi contains a ButtonTable', function () {
+        const testObject = EditAnswerTestUtils.createTestObject(answerConfigMulti, answer);
+        const divButtonTable = testObject.element.querySelector("#tableButtonContainer")
+        expect(divButtonTable).to.be.not.null
+    });
+})
 
