cb2d2d87fe2c09b3de0c3dc29488faec318be6be
d.reichler <d.reichler@hsag.info>
2022-12-19 09:59:49 +0100

    [TASK] removed wrong async statement of api method

--- src/api/virtualIntent.js
+++ src/api/virtualIntent.js
@@ -1,7 +1,7 @@
 import request from '@/utils/request'
 import { authorizedBackendUrl } from '@/utils/backendUrl'
 
-export async function getVirtualIntents() {
+export function getVirtualIntents() {
     return request({
         url: authorizedBackendUrl + "getVirtualIntents",
         method: "get"
