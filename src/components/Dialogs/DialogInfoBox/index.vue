2e58f166f877e0c1a02de594d9bbb86a129fff9f
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2023-03-13 16:00:55 +0100

    [TASK] pass technicalIntentName into startDialog WebchatApi

--- src/components/Dialogs/DialogInfoBox/index.vue
+++ src/components/Dialogs/DialogInfoBox/index.vue
@@ -38,7 +38,7 @@ export default {
   },
   methods: {
     startDialogForcurrentIntent() {
-      new WebchatApi().startDialog(this.intent)
+      new WebchatApi().startDialog(this.technicalIntentName)
     },
     openLink(link) {
       window.open(link, "_blank");
