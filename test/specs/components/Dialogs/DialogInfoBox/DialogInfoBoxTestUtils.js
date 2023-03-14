2d8146d9a084c206202f831df053e9c22c8a77c4
HSAG\m.gwiozda <m.gwiozda@hsag.info>
2023-03-14 08:42:38 +0100

    [REFACTOR] extracted createTestObject - function in DialogInfoBoxTestUtils

--- test/specs/components/Dialogs/DialogInfoBox/DialogInfoBoxTestUtils.js
+++ test/specs/components/Dialogs/DialogInfoBox/DialogInfoBoxTestUtils.js
@@ -1,15 +1,26 @@
 import DialogInfoBox from '../../../../../src/components/Dialogs/DialogInfoBox/index.vue';
 import VueTestUtils from '../../../../utils/VueTestUtils';
 
+/**
+ * @typedef {import('@vue/test-utils')['mount']} mount
+ * @typedef {Parameters<mount>[1]} Options
+ */
 export default class DialogInfoBoxTestUtils {
     createTestObjectWithDescription(description) {
-        const testUtils = this.#createVueTestUtil();
-        return testUtils.createTestObject({
+        return this.createTestObject({
             props: {
                 description,
                 entity: {}
-            }
+            },
         });
+    }
+
+    /**
+     * @param {Options} options
+     */
+    createTestObject(options) {
+        const testUtils = this.#createVueTestUtil();
+        return testUtils.createTestObject(options);
     }
 
     #createVueTestUtil() {
