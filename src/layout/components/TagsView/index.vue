d858f9e1358cfa7ce82b4542b646cea101020130
Till Herpertz <t.herpertz@hsag.info>
2023-07-25 09:32:08 +0200

    [CHAT-1381] Removed stylings for the old close icon pseudo element

--- src/layout/components/TagsView/index.vue
+++ src/layout/components/TagsView/index.vue
@@ -276,27 +276,6 @@ export default {
     i {
       margin-left: 5px;
     }
-
-    .el-icon-close {
-      width: 16px;
-      height: 16px;
-      vertical-align: 2px;
-      border-radius: 50%;
-      text-align: center;
-      transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
-      transform-origin: 100% 50%;
-
-      &:before {
-        transform: scale(0.6);
-        display: inline-block;
-        vertical-align: -3px;
-      }
-
-      &:hover {
-        background-color: #b4bccc;
-        color: #fff;
-      }
-    }
   }
 }
 </style>
