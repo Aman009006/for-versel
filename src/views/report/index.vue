4fdc5b4bdff32d8d96e8a0de34737fe36a38026c
Till Herpertz <t.herpertz@hsag.info>
2023-07-26 13:50:08 +0200

    [CHAT-1707] Added Report view component to display powerbi report in iframe

---
+++ src/views/report/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <iframe :title="customer" :src="combinedLink" frameborder="0" allowFullScreen="true">
+    </iframe>
+</template>
+
+<script>
+export default {
+    name: "Report",
+    components: {},
+    props: {
+        customer: {
+            type: String,
+            required: true
+        },
+        powerBiReportId: {
+            type: String,
+            required: true
+        }
+    },
+    created() {
+        this.combinedLink = `https://app.powerbi.com/reportEmbed?reportId=${this.powerBiReportId}&autoAuth=true`;
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style>
+iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
