f12b61c12c654e1590b77a8e40b60444d84331b7
Till Herpertz <t.herpertz@hsag.info>
2023-08-10 11:46:30 +0200

    [CHAT-841] removed inline styling for sidebar

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -4,10 +4,7 @@
       <el-menu
         :default-active="activeMenu"
         :collapse="isCollapse"
-        :background-color="variables.menuBg"
-        :text-color="variables.menuText"
         :unique-opened="false"
-        :active-text-color="variables.menuActiveText"
         :collapse-transition="false"
         mode="vertical"
       >
