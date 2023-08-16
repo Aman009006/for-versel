a2231a00043985c0ccbafa775db9704b4f673d58
Till Herpertz <t.herpertz@hsag.info>
2023-08-16 12:28:53 +0200

    [CHAT-841] added absolute position to prevent scrollbar on mainapp

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -35,11 +35,11 @@ export default {
 @import "@/styles/variables.module.scss";
 
 .app-main {
+  position: absolute;
   min-height: calc(100vh - $navBarHeight);
   width: calc(100% - $sideBarWidth);
   margin-top: $navBarHeight;
   margin-left: $sideBarWidth;
-  position: relative;
   overflow: hidden;
   transition: margin-left 0.28s;
 }
