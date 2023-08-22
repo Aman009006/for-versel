ce757a2ed9603e66c8d6ceea00abb13b16541751
Till Herpertz <t.herpertz@hsag.info>
2023-08-22 08:38:47 +0200

    [CHAT-842] changed way hover is triggered and positioned

--- src/views/intents/intent-group/index.vue
+++ src/views/intents/intent-group/index.vue
@@ -2,31 +2,18 @@
   <div>
     <intentGroupInfobox></intentGroupInfobox>
     <intentSearch :searchableArray="intents" @filteredArray="updateIntents"></intentSearch>
-    <div id="intent-hover" class="hidden">
-      <h3>Antwort Vorschau:</h3>
-      <p class="intent-hover-text"></p>
+    <div id="intent-hover" class="hidden" @mouseover="displayHover">
+      <div class="intent-hover-text"></div>
     </div>
-    <el-table :data="filteredArray" stripe>
-      <el-table-column label="Name">
-        <template #default="scope">
-          <el-row @mousemove="positionHoverAtMouse" @mouseover="displayHover(scope.row)" @mouseout="hideHover()">
-            {{ scope.row.name }}
-          </el-row>
-        </template>
+    <el-table :data="filteredArray" stripe @cell-mouse-enter="displayHover" @cell-mouse-leave="hideHover">
+      <el-table-column prop="name" label="Name">
       </el-table-column>
-      <el-table-column label="Beispiele / Beschreibung">
-        <template #default="scope">
-          <el-row @mousemove="positionHoverAtMouse" @mouseover="displayHover(scope.row)" @mouseout="hideHover()">
-            {{ scope.row.utterances }}
-          </el-row>
-        </template>
+      <el-table-column prop="utterances" label="Beispiele / Beschreibung">
       </el-table-column>
       <el-table-column label="Aktion">
-        <template #default="scope">
-          <el-button @mousemove="positionHoverAtMouse" @mouseover="displayHover(scope.row)" @mouseout="hideHover()">
-            Öffnen
-          </el-button>
-        </template>
+        <el-button>
+          Öffnen
+        </el-button>
       </el-table-column>
     </el-table>
   </div>
@@ -62,24 +49,18 @@ export default {
     displayHover(row) {
       const intentContainer = document.getElementById('intent-hover');
       const intentContainerText = intentContainer.getElementsByClassName('intent-hover-text')[0];
-      this.positionHoverAtMouse(event);
       intentContainer.classList.remove('hidden');
       if (row.texts) {
         intentContainerText.innerHTML = md.render(
           this.truncateString(row.texts[0].text, 20)
         );
       } else {
-        intentContainerText.innerHTML = 'Keine Antwort hinterlegt...';
+        intentContainerText.innerHTML = '<p>Keine Antwort hinterlegt...</p>';
       }
     },
     hideHover() {
       const intentContainer = document.getElementById('intent-hover');
       intentContainer.classList.add('hidden');
-    },
-    positionHoverAtMouse(e) {
-      const intentContainer = document.getElementById('intent-hover');
-      intentContainer.style.top = (e.clientY - 50) + 'px';
-      intentContainer.style.left = (e.clientX - 200) + 'px';
     },
     truncateString(string, maxWords) {
       const array = string.split(" ");
@@ -98,16 +79,21 @@ export default {
 
 <style lang="scss" scoped>
 #intent-hover {
-  position: absolute;
-  width: auto;
-  max-width: 40%;
-  height: auto;
+  position: fixed;
+  bottom: 20px;
+  width: 70%;
+  height: 100px;
   background-color: #fff;
   border: 1px solid #ccc;
   border-radius: 5px;
-  padding: 10px;
+  padding: 5px;
   overflow-y: hidden;
   z-index: 100;
+
+  h3,
+  p {
+    margin: 0;
+  }
 }
 
 .hidden {
