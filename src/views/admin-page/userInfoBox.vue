dd35e60417fc45d85a48e410abf6e2ec15d762d6
a.mamashov <a.mamashovfend.extern@hsag.info>
2024-02-02 15:17:25 +0600

    asdas

--- src/views/admin-page/userInfoBox.vue
+++ src/views/admin-page/userInfoBox.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="userInfoBox infoBox">
-    <el-row>
+    <div>
       <h1>Benutzerverwaltung</h1>
       <span>
         <p style="font-size: 12px;">
           Administratoren haben Zugriff auf Funktionen zur Erstellung, Bearbeitung und Löschung von Benutzerkonten. Sie können Benutzernamen, E-Mail-Adressen und Passwörter verwalten.
             Es ist möglich Konten zu sperren oder entsperren.
         </p>
-
         <p style="font-size: 12px">
           Die Übersicht bietet eine nahtlose Verwaltung der Benutzerkonten, um einen reibungslosen Ablauf im System zu gewährleisten.
         </p>
-
       </span>
-    </el-row>
+    </div>
   </div>
 </template>
